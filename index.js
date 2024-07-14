df43a421c371f787e86ac2339cddeb217bac05cf
MissMurigi <vickymurigi001@gmail.com>
2024-07-14 07:01:08 +0300

    E.L

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
+  const input = document.getElementById("button"); // Assuming there's an element with id 'button'
+
+  function clickAlert() {
+    alert("I was clicked!");
+  }
+
+  input.addEventListener("click", clickAlert);
 }
+
+addingEventListener(); // Call the function to set up the event listener
